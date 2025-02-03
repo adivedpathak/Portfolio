1be679451018904bbdd8374b7fed3d5017b49f48
Mihir <mihir.pande22@pccoepune.org>
2025-02-03 16:55:03 +0530

    initial commit

---
+++ src/sections/Header.tsx
@@ -0,0 +1,15 @@
+export const Header = () => {
+  return (
+    <div className="flex justify-center items-center fixed top-3 w-full">
+      <div className="flex justify-center w-full">
+        <nav className="flex gap-2 p-1 border border-white/15 rounded-full bg-white/10 backdrop-blur w-max">
+
+          <a href="#" className="nav-item text-sm sm:text-base px-3 sm:px-4 py-2">Home</a>
+          <a href="#" className="nav-item text-sm sm:text-base px-3 sm:px-4 py-2">Projects</a>
+          <a href="#" className="nav-item text-sm sm:text-base px-3 sm:px-4 py-2">About</a>
+          <a href="#" className="nav-item text-sm sm:text-base px-3 sm:px-4 py-2 bg-white text-gray-900 hover:bg-white/90">Content</a>
+        </nav>
+      </div>
+    </div>
+  );
+};
