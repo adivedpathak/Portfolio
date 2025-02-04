ef349aa2bad329455485d15eb4d30a69be17df58
Mihir <mihir.pande22@pccoepune.org>
2025-02-04 21:32:28 +0530

    added 4th card

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -45,31 +45,45 @@ const toolBoxItems = [
 const hobbies = [
   {
     title: 'Painting',
-    emoji: '🎨'
+    emoji: '🎨',
+    left: '5%',
+    top: '5%',
   },
   {
     title: 'Photography',
-    emoji: '📸'
-  },
-  {
-    title: 'Gaming',
-    emoji: '🎮'
+    emoji: '📸',
+    left: '50%',
+    top: '5%',
   },
   {
     title: 'Hiking',
-    emoji: '🥾'
+    emoji: '🥾',
+    left: '35%',
+    top: '40%',
+  },
+  {
+    title: 'Gaming',
+    emoji: '🎮',
+    left: '10%',
+    top: '35%',
   },
   {
     title: 'Music',
-    emoji: '🎵'
+    emoji: '🎵',
+    left: '70%',
+    top: '45%',
   },
   {
     title: 'Reading',
-    emoji: '📚'
+    emoji: '📚',
+    left: '45%',
+    top: '70%',
   },
   {
     title: 'Fitness',
-    emoji: '🏋️'
+    emoji: '🏋️',
+    left: '5%',
+    top: '65%',
   },
 ]
 
@@ -89,12 +103,15 @@ export const AboutSection = () => {
         <ToolBoxItems items={toolBoxItems} className="mt-6"/>
         <ToolBoxItems items={toolBoxItems} className="mt-6" itemsWrapperClassName="-translate-x-1/2"/>
         </Card>
-        <Card>
-        <CardHeader title="Beyond the Code" description="Explore my interests and hobbies beyond the digital relm." />
-          <div>
+        <Card className="h-[320px] p-0 flex flex-col">
+        <CardHeader title="Beyond the Code" description="Explore my interests and hobbies beyond the digital relm." className="px-6 py-6"/>
+          <div className="relative flex-1">
             {hobbies.map(hobby => (
-              <div key={hobby.title}>
-                <span>{hobby.title}</span>
+              <div key={hobby.title} className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute" style={{
+                left: hobby.left,
+                top: hobby.top,
+              }}>
+                <span className="font-medium text-gray-950">{hobby.title}</span>
                 <span>{hobby.emoji}</span>
               </div>
             ))}
