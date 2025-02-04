e9db77512e645371e988284a3174e28203bb5dd7
Mihir <mihir.pande22@pccoepune.org>
2025-02-04 21:37:21 +0530

    finished cards need to make responsive

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -117,9 +117,11 @@ export const AboutSection = () => {
             ))}
           </div>
         </Card>
-        <Card>
-            <Image src={MapImage} alt="Map" />
-            <Image src={SmileMemoji} alt="Smile Memoji" />
+        <Card className="h-[320px] p-0 relative">
+            <Image src={MapImage} alt="Map" className="h-full w-full object-cover object-left-top" />
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+              <Image src={SmileMemoji} alt="Smile Memoji" className="size-20" />
+            </div>
         </Card>
       </div>
     </div>
