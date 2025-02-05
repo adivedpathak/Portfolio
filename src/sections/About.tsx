37526fe8c34bbdc93601ab159d0b54a7bc6184a6
Mihir <mihir.pande22@pccoepune.org>
2025-02-05 11:17:40 +0530

    finished the project now time to add personalization

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,6 +1,6 @@
+'use client';
 import { SectionHeader } from "@/components/SectionHeader";
 import { Card } from "@/components/Card";
-import StarIcon from "@/assets/icons/star.svg";
 import BookImage from "@/assets/images/book-cover.png";
 import Image from "next/image";
 import JavascriptIcon from '@/assets/icons/square-js.svg'
@@ -10,10 +10,11 @@ import ReactIcon from '@/assets/icons/react.svg'
 import ChromeIcon from '@/assets/icons/chrome.svg'
 import GithubIcon from '@/assets/icons/github.svg'
 import MapImage from '@/assets/images/map.png'
-import { TechIcon } from "@/components/TechIcon";
 import SmileMemoji from '@/assets/images/memoji-smile.png'
 import { CardHeader } from "@/components/CardHeader";
 import { ToolBoxItems } from "@/components/ToolBoxItems";
+import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const toolBoxItems = [
   {
@@ -88,6 +89,7 @@ const hobbies = [
 ]
 
 export const AboutSection = () => {
+  const constraintRef = useRef(null);
   return <div className="py-20 lg:py-28">
     <div className="container">
       <SectionHeader eyebrow="About me" title="A Glimpse Into my World" description="Learn more about who I am, what I do, and what inspires me." />
@@ -101,28 +103,32 @@ export const AboutSection = () => {
           </Card>
           <Card className="h-[320px] md:col-span-3 lg:col-span-2">
             <CardHeader title="My Toolbox" description="Explore the technologies and tools I use to craft digital experiences." className=""/>
-            <ToolBoxItems items={toolBoxItems} className=""/>
-            <ToolBoxItems items={toolBoxItems} className="mt-6" itemsWrapperClassName="-translate-x-1/2"/>
+            <ToolBoxItems items={toolBoxItems} className="" itemsWrapperClassName="animate-move-left [animation-duration:30s]"/>
+            <ToolBoxItems items={toolBoxItems} className="mt-6" itemsWrapperClassName="animate-move-right [animation-duration:15s]"/>
           </Card>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 gap-8">
           <Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
           <CardHeader title="Beyond the Code" description="Explore my interests and hobbies beyond the digital relm." className="px-6 py-6"/>
-            <div className="relative flex-1">
+            <div className="relative flex-1" ref={constraintRef}>
               {hobbies.map(hobby => (
-                <div key={hobby.title} className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute" style={{
+                <motion.div key={hobby.title} className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute" style={{
                   left: hobby.left,
                   top: hobby.top,
-                }}>
+                }} drag
+                dragConstraints={constraintRef}>
+                  
                   <span className="font-medium text-gray-950">{hobby.title}</span>
                   <span>{hobby.emoji}</span>
-                </div>
+                </motion.div>
               ))}
             </div>
           </Card>
           <Card className="h-[320px] p-0 relative md:col-span-2 lg:col-span-1">
               <Image src={MapImage} alt="Map" className="h-full w-full object-cover object-left-top" />
-              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+              <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-20 animate-ping [animation-duration:2s]"></div>
+              <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-10"></div>
                 <Image src={SmileMemoji} alt="Smile Memoji" className="size-20" />
               </div>
           </Card>
