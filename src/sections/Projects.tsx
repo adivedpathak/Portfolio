fb16786aa67b0a3ef42c5c027c6c6a3fa88b875b
Mihir <mihir.pande22@pccoepune.org>
2025-02-05 09:55:31 +0530

    added base animation to hero

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -6,7 +6,7 @@ import CheckCircleIcon from "@/assets/icons/check-circle.svg";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 import GrainImage from "@/assets/images/grain.jpg";
 import { SectionHeader } from "@/components/SectionHeader";
-import { Card } from "@/components/Card"; 
+import { Card } from "@/components/Card";
 
 const portfolioProjects = [
   {
@@ -48,44 +48,59 @@ const portfolioProjects = [
 ];
 
 export const ProjectsSection = () => {
-  return <section className="pb-16 lg:py-24">
-    <div className="container">
-      <SectionHeader eyebrow="Real-world Results" title="Featured Projects" description="See how I transformed repetative tasks into engaging digital experiences."/>
-      <div className="mt-10 md:mt-20 flex flex-col gap-20">
-        {portfolioProjects.map(project => (
-          <Card className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20">
-            <div className="lg:grid lg:grid-cols-2 lg:gap-16">
-              <div className="lg:pb-16">
-              <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
-                <span>{project.company}</span>
-                <span>•</span>
-                <span>{project.year}</span>
+  return (
+    <section className="pb-16 lg:py-24">
+      <div className="container">
+        <SectionHeader
+          eyebrow="Real-world Results"
+          title="Featured Projects"
+          description="See how I transformed repetative tasks into engaging digital experiences."
+        />
+        <div className="mt-10 md:mt-20 flex flex-col gap-20">
+          {portfolioProjects.map((project) => (
+            <Card
+              key={project.link}
+              className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20"
+            >
+              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                <div className="lg:pb-16">
+                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                    <span>{project.company}</span>
+                    <span>•</span>
+                    <span>{project.year}</span>
+                  </div>
+                  <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">
+                    {project.title}
+                  </h3>
+                  <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                  <hr />
+                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                    {project.results.map((result) => (
+                      <li className="flex gap-2 text-sm md:text-base text-white/50">
+                        <CheckCircleIcon className="size-5 md:size-6" />
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+                  <a href={project.link}>
+                    <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                      <span>Visit Live Site</span>
+                      <ArrowUpRightIcon className="size-4" />
+                    </button>
+                  </a>
+                </div>
+                <div className="relative">
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
+                  />
+                </div>
               </div>
-              <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">{project.title}</h3>
-              <hr className="border-t-2 border-white/5 mt-4 md:mt-5"/>
-              <hr />
-              <ul className="flex flex-col gap-4 mt-4 md:mt-5">
-                {project.results.map((result) => (
-                  <li className="flex gap-2 text-sm md:text-base text-white/50">
-                    <CheckCircleIcon className="size-5 md:size-6"/>
-                    <span>{result.title}</span>
-                  </li>
-                ))}
-              </ul>
-              <a href={project.link}>
-                <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
-                  <span>Visit Live Site</span>
-                  <ArrowUpRightIcon className="size-4"/>
-                </button>
-              </a>
-              </div>
-              <div className="relative">
-                <Image src={project.image} alt={project.title} className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"/>
-              </div>
-            </div>
-          </Card>
-        ))}
+            </Card>
+          ))}
+        </div>
       </div>
-    </div>
-  </section>;
+    </section>
+  );
 };
